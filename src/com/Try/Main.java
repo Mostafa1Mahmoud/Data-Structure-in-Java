cd683ba1c83594d00cae7cedc36d1136ad024796
Mostafa Mahmoud <mmossttafa@gmail.com>
2021-01-14 19:17:36 +0200

    Clearing main

--- src/com/Try/Main.java
+++ src/com/Try/Main.java
@@ -9,17 +9,5 @@ import java.util.Scanner;
 * */
 public class Main {
     public static void main(String[] args) {
-        double_list list = new double_list();
-        for(int i = 0; i < 10 ;i++)list.push_back(i);
-        list.Reverse();
-        list.print_front();
-        System.out.println("------------");
-        for(int i = 0;i < 5;i++)list.push_front(i);
-        list.print_front();
-        System.out.println("------------");
-        list.Insertion_sort();
-        list.print_front();
-        System.out.println("------------");
-        list.print_back();
     }
 }
